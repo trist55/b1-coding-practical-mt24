4bba20a58016faf667eb3f984728c6f0ef3a9bba
trist55 <tristan.chia@hmc.ox.ac.uk>
2024-10-23 03:19:50 +0100

    Fixed Controller (added sense of self)

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -6,11 +6,11 @@ class Controller:
         self.K_p = 0.15
         self.K_d = 0.6
         
-    def calculate_error(reference, depth):
+    def calculate_error(self, reference, depth):
         
         error = reference - depth
         return error
     
     def PD_controller(self, reference, depth, error_previous):
-        error_current = Controller.calculate_error(reference, depth)
+        error_current = Controller.calculate_error(self, reference, depth)
         action = self.K_p * error_current + self.K_d * (error_current - error_previous)
