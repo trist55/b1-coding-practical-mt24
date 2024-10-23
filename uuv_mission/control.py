6a585cf6ae7ccf69607afacedc3592ac5d0de247
trist55 <tristan.chia@hmc.ox.ac.uk>
2024-10-23 03:39:58 +0100

    Returned

--- uuv_mission/control.py
+++ uuv_mission/control.py
@@ -14,3 +14,4 @@ class Controller:
     def PD_controller(self, reference, depth, error_previous):
         error_current = Controller.calculate_error(self, reference, depth)
         action = self.K_p * error_current + self.K_d * (error_current - error_previous)
+        return action
